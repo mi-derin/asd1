99ef4532f54b73bf64ddc487ec37793a3066fa71
Derin <mi.derinn@gmail.com>
2024-01-04 23:53:52 +0300

    Implement feature

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def main():
     print("Hello, this is the main branch!")
 
+def feature_function():
+    print("This is a feature added in the feature branch.")
+
 if __name__ == "__main__":
     main()
